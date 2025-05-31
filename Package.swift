fb5ebd657312219845568aa830f804376489e677
Ratnesh Jain <ratneshjain1993@gmail.com>
2025-05-31 11:42:59 +0530

    Fix the target dependency for DocumentScannerFeature.

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ let package = Package(
         ),
         .target(
             name: "DocumentScannerFeature",
-            dependencies: [.tca]
+            dependencies: ["DefaultStorageDirectory"]
         ),
     ]
 )
